68a16b18fa914b4dce48f583519e84d2a7b31e4a
xinjie <xinjie6016@gmail.com>
2023-02-07 18:05:27 +0800

    update

--- src/y2018/Q4.java
+++ src/y2018/Q4.java
@@ -1,14 +1,13 @@
 package y2018;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.text.DecimalFormat;
 import java.util.Scanner;
 import java.util.Arrays;
+
 public class Q4 {
     public static void main(String[] args) {
         String winner = "";
         double max = 0;
-        DecimalFormat df = new DecimalFormat("#.##");
         try{
         Scanner sc = new Scanner(new FileInputStream("Q4.txt"));
         while(sc.hasNextLine()){
@@ -33,7 +32,7 @@ public class Q4 {
                 max = total;
                 winner = col[0];
             }
-            System.out.println(col[0] + " score " + df.format(total));
+            System.out.printf("%s score %.2f\n",col[0],total);
         }
         sc.close();
         System.out.println(winner + " is the winner");
